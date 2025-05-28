4d488364f3097ecd4fa8c445c1ba416322af7edf
Ebunoluwa Owoeye <ebunowoeye316@gmail.com>
2025-05-28 22:12:31 +0100

    login fix

--- routes/users.js
+++ routes/users.js
@@ -21,27 +21,28 @@ const bcrypt = require('bcrypt');
     const user = await newUser.save();
     res.status(200).json(user._id);
   } catch (error) {
-    res.status(500).json(err)
+    res.status(500).json(error)
   }
  })
 
+ 
 //login
-
 router.post("/login", async(req,res) => {
   try {
     //find user
-    const user = await User.findOne({username:req.body.username})
-    !user && res.status(400).json("Wrong username or password!")
+    const user = await User.findOne({username:req.body.username});
+    if (!user) return res.status(400).json("Wrong username or password!");
 
     //validate password
-     const validPassword = await bcrypt.compare(req.body.password, user.password);
-     !validPassword && res.status(400).json("Wrong username or password!")
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).json("Wrong username or password!");
+
     //send res
-     res.status(200).json({_id:user._id, username: user.username})
+    res.status(200).json({_id:user._id, username: user.username});
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
-})
+});
 
 module.exports = router;
 
